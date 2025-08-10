90efc47aab6757f8ad8e677b86dcf9a6843f3394
Jake McCrary <jake@jakemccrary.com>
2025-08-10 18:20:41 -0500

    Fix service worker path for subdirectory deployment

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = 'place-notes-v1';
 const ASSETS = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/app.js',
-  '/manifest.webmanifest',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
